bc1e3e50d9e9a005ed1066dca64a8729cfc9a0e2
hsinlun99 <hsinlun.chiu@gmail.com>
2025-02-09 01:26:20 +0100

    feat: :sparkles: congrats UI pop up when arriving the finish block

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject congratsUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            Debug.Log("Congrats");
+            congratsUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+}
